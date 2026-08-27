4f4de322139af7b9ef8988df79ba863a0be6c1b9
terminal <rajsharma26125@gmail.com>
2026-08-27 22:08:57 +0530

    main server file code change

--- app.js
+++ app.js
@@ -4,17 +4,20 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const connectDB = require("./config/DB");
 const authRoutes = require('./routes/authRoutes');
+const path = require('path');
 
 connectDB();
 
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
+// const port = 8080;
 
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
+// app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api/auth', authRoutes);
 
 
@@ -24,6 +27,10 @@ app.get('/', (req, res) => {
 
 
 
-app.listen(8080, () => {
+// app.listen(8080, () => {
+//     console.log(`Server is running on ${port}`);
+// });
+
+app.listen(port, () => {
     console.log(`Server is running on ${port}`);
 });
